25b8233f2340d8988f3a153bb951b18684c8eafa
senichek <senichek@gmail.com>
2026-01-04 17:38:55 +0100

    added VPC, elastic IP, subnets and route tables

---
+++ terraform/modules/VPC/variables.tf
@@ -0,0 +1,6 @@
+variable "vpc_cidr_block" {}
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
+variable "availability_zones" { type = list(string) }
+variable "project" {}
+variable "environment" {}
